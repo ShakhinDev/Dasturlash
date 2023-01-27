4aeb01b2a4c04cdbdb300011713ac9982af9dea0
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-01-27 01:28:50 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+def calculator():
+    while True:
+        print("Select operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+        choice = input("Enter choice(1/2/3/4/5): ")
+        if choice in ['1', '2', '3', '4', '5']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(num1 + num2)
+            elif choice == '2':
+                print(num1 - num2)
+            elif choice == '3':
+                print(num1 * num2)
+            elif choice == '4':
+                if num2 != 0:
+                    print(num1 / num2)
+                else:
+                    print("Error: Division by zero")
+            elif choice == '5':
+                break
+        else:
+            print("Invalid Input")
+calculator()
