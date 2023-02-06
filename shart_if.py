950f4211af935f13389c918425becdfd69b9b80c
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-02-06 19:07:21 +0100

    Add files via upload

---
+++ shart_if.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+yosh = int(input("Yoshingizni kiriting>>> "))
+if yosh>=18:
+    print("Xush kelibsiz!")
+else:
+    print("Tur yuqol!")
+
+
+# In[4]:
+
+
+yosh<19
+
+
+# In[5]:
+
+
+yosh<16
+
+
+# In[7]:
+
+
+login=input("Loginingizi kiriting>> ")
+if len(login)<=5:
+    print("Login 5 ta belgidan ko'p bo'lishi kerak")
+else:
+    print(f"Loginingniz {login} bilam ma'lumotlar bazamizda saqlandi ")
+
+
+# In[14]:
+
+
+yil = int(input("Tugilgan yilingizni kiriting: "))
+if 2023-yil<18:
+    print(f"yoshingiz {2023-yil}da ekan ")
+    print("Sizga kirish mumkin emas tur yuqoling !!!")
+else:
+    print(f"Yoshingiz {2023-yil} da ekan !!!")
+    print("Xush kelibsiz mazza qilib hordiq chiqaring!!!")
+
+
+# In[ ]:
+
+
+
+
